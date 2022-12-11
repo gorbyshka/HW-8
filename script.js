73e238c41d46dcac15a65baa8316c2418fe20e29
GoRbYsKkA <gleb.kovalchuk0609@gmail.com>
2022-12-11 19:48:36 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const numOrStr = prompt('input number or string');
+console.log(numOrStr);
+
+switch (numOrStr) {
+    case null:
+        console.log('ви скасували');
+        break;
+    case ' ':
+    case '':
+        console.log('empty string');
+        break;
+    case !isNaN(numOrStr) || numOrStr:
+        console.log('number is Ba_NAN');
+        break;
+    default:
+        console.log('ok');
+        break;
+}
